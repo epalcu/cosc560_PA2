cbe6c7463770c5e720ec2e6d2c9ea1c5518647c8
epalcu <elliaspalcu@yahoo.com>
2018-02-03 18:18:28 -0500

    Update p1_mapper.py

--- p1_mapper.py
+++ p1_mapper.py
@@ -4,11 +4,13 @@ import os
 import re
 
 class Mapper():
+    
     # Read in contents from file via stdin
     def make_pairs(self):
         line_num = 1
         for input in sys.stdin:
             input = re.findall(r"(?<![@#])\b\w+(?:'\w+)?", input)
+            
             # For each word, return (key, value) pair of its lowercase form and trivial value of 1
             # Print to stdout for reducer to read in
             for word in input:
